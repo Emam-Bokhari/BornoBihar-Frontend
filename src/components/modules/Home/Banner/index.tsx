64c5d4d6a20647540fd59781d1619bd30816c7d3
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-25 13:59:35 +0600

    chore: change button padding

--- src/components/modules/Home/Banner/index.tsx
+++ src/components/modules/Home/Banner/index.tsx
@@ -17,16 +17,16 @@ export default function BannerSection() {
           {/* Overlay */}
         </div>
         <div className="relative z-10 text-center">
-          <h1 className="text-4xl font-bold mb-4 text-white">
+          <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold mb-4 text-white">
             Your Gateway to the Best Books
           </h1>
-          <p className="text-lg mb-6 text-white">
+          <p className="text-base md:text-lg mb-6 text-white">
             Explore a diverse collection of books across every genre. Whether
             you’re an avid reader or just getting started,
             <br />
             we have something for everyone.
           </p>
-          <Button className="px-6 py-3 bg-[#F65D4E] text-white font-semibold shadow-lg hover:bg-[#D84C3F] cursor-pointer">
+          <Button className=" bg-[#F65D4E] text-white   hover:bg-[#D84C3F] cursor-pointer">
             Start Your Reading Journey
           </Button>
         </div>
