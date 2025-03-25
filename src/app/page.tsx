48f926cbd39b82b36c00edea12893d8ecd15f709
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-25 12:26:22 +0600

    init: initialize project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Fragment } from "react";
+
+export default function HomePage() {
+  return (
+    <Fragment>
+      <p>Home page</p>
+      <Button>Click Me</Button>
+    </Fragment>
+  );
+}
