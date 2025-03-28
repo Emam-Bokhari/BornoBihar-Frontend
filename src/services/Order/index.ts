f01b5d59728d18bb2bffcb39327408ea2b7f1d82
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-28 13:07:45 +0600

    feat: add orders data fetchs, and updating status function

--- src/services/Order/index.ts
+++ src/services/Order/index.ts
@@ -48,3 +48,61 @@ export const getOrderHistory = async () => {
         throw new Error(error)
     }
 };
+
+export const getAllOrders = async () => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/orders`, {
+            next: {
+                // revalidate: 30,
+                tags: ["ORDER"]
+            },
+            method: "GET",
+            headers: {
+                "Authorization": (await cookies()).get("accessToken")!.value,
+            }
+        });
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
+
+
+export const getOrderById = async (id: string) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/orders/${id}`, {
+            // cache: "no-store",
+            next: {
+                tags: ["ORDER"]
+            },
+            method: "GET",
+            headers: {
+                "Authorization": (await cookies()).get("accessToken")!.value,
+            }
+        });
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
+
+export const updateOrderStatusById = async (id: string, status: any) => {
+    try {
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/orders/${id}/status`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": (await cookies()).get("accessToken")!.value,
+            },
+            body: JSON.stringify(status)
+        })
+        revalidateTag("");
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
