4f226c536d932fe1094ecc76fcea031366f5df80
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-28 11:22:13 +0600

    feat: implement user management table with role, status update, and delete functionalit

--- src/app/(WithDashboardLayout)/admin/dashboard/users/page.tsx
+++ src/app/(WithDashboardLayout)/admin/dashboard/users/page.tsx
@@ -1,11 +1,10 @@
 export const dynamic = "force-dynamic";
-import ManageUsers from "@/components/modules/Dashboard/Users";
+import ManageUsers from "@/components/modules/Dashboard/Admin/Users";
 import { getAllUsers } from "@/services/User";
 
-export default async function ProductsManagementPage() {
+export default async function UsersManagementPage() {
   const { data } = await getAllUsers();
-  const users = data.result ?? [];
-
+  const users = data ?? [];
   return (
     <div className="p-4">
       <ManageUsers users={users} />
